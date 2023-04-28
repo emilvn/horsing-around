986865e3d408e069f27f78e641477c8a277d2582
emilvn <emilvnielsen@hotmail.com>
2023-04-28 19:33:35 +0200

    added search functions

--- modules/search.js
+++ modules/search.js
@@ -1,5 +1,17 @@
 "use strict";
 
 /* ========== SEARCH BY NAME/RACE/COLOR ========== */
-//todo add inputSearchChanged here
-//todo add searchHorses here
+import {horseArr, showHorses} from "./display.js";
+
+export function inputSearchChanged(event){
+    const searchValue = event.target.value;
+    const filteredHorses = searchHorses(searchValue);
+    showHorses(filteredHorses);
+}
+function searchHorses(searchValue){
+    return horseArr.filter((horse)=>
+    horse["name"].includes(searchValue)
+            || horse["race"].includes(searchValue)
+            || horse["color"].includes(searchValue)
+    );
+}
