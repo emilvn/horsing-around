b82834e632b6ea2b11da8c383efbd5dcc6adeb5f
emilvn <emilvnielsen@hotmail.com>
2023-04-30 23:53:16 +0200

    fixed search indentation

--- modules/search.js
+++ modules/search.js
@@ -4,15 +4,15 @@
 import { horseArr, showHorses } from "./display.js";
 
 export function inputSearchChanged(event) {
-  const searchValue = event.target.value;
-  const filteredHorses = searchHorses(searchValue);
-  showHorses(filteredHorses);
+    const searchValue = event.target.value;
+    const filteredHorses = searchHorses(searchValue);
+    showHorses(filteredHorses);
 }
 function searchHorses(searchValue) {
-  return horseArr.filter(
-    (horse) =>
-        (horse["name"].toLowerCase().includes(searchValue.toLowerCase())
-            || horse["race"].toLowerCase().includes(searchValue.toLowerCase())
-            || horse["color"].toLowerCase().includes(searchValue.toLowerCase()))
-  );
+    return horseArr.filter(
+        (horse) =>
+            (horse["name"].toLowerCase().includes(searchValue.toLowerCase())
+                || horse["race"].toLowerCase().includes(searchValue.toLowerCase())
+                || horse["color"].toLowerCase().includes(searchValue.toLowerCase()))
+    );
 }
