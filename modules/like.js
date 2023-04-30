358369cdb2d2bd600e3d23280578ee120791fb26
emilvn <emilvnielsen@hotmail.com>
2023-04-30 23:54:26 +0200

    fixed indentation

--- modules/like.js
+++ modules/like.js
@@ -28,23 +28,23 @@ async function updateLikes(likesAmount, horseID, endpoint, button) {
     // location is "detailDialog" or "horseGrid"
     try {
     const response = await fetch(`${endpoint}horses/${horseID}.json`, {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        likes: likesAmount,
-      }),
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            likes: likesAmount,
+        }),
     });
     if (response.ok) {
-      showToastMessage("Horse liked/disliked", "success");
-      console.log("Horse like added successfully");
-      await displayUpdatedLikes(button, horseID);
+        showToastMessage("Horse liked/disliked", "success");
+        console.log("Horse like added successfully");
+        await displayUpdatedLikes(button, horseID);
     } else {
-      showToastMessage("Horse like/dislike failed", "error");
-      console.log("Bad response at updateLikes");
+        showToastMessage("Horse like/dislike failed", "error");
+        console.log("Bad response at updateLikes");
     }
-  } catch (err) {
-    throw new Error(`Error at updateLikes: ${err}`);
-  }
+    } catch (err) {
+        throw new Error(`Error at updateLikes: ${err}`);
+    }
 }
 async function displayUpdatedLikes(button, horseID){
     const horseObj = await getOneHorse(horseID, endpoint);
