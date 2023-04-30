2a8f7eef6cc5560845502f890492b188373c7e6b
TheDanishMexican <danieljchristjansen@gmail.com>
2023-04-30 13:37:43 +0200

    added const password

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ window.addEventListener("load", main);
 
 
 /* ===== Global variables ===== */
+export const password = "emptyyourselfinsideme123"
 export const endpoint =
   "https://gallopgalore-80085-default-rtdb.europe-west1.firebasedatabase.app/";
 
