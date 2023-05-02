a3ae5a9715d84a3b0bd145e045465f2182b82805
emilvn <emilvnielsen@hotmail.com>
2023-05-02 23:41:07 +0200

    createdialog fixed

--- js/modules/dialogs/createDialog.js
+++ js/modules/dialogs/createDialog.js
@@ -1,12 +1,12 @@
 import { submitCreateForm } from "../submit/submit.js";
 /* ========== CREATE DIALOG ========== */
 export function showCreateDialog() {
-  const form = document.querySelector("#create-form");
-  form.parentElement.showModal();
-  form.addEventListener("submit", submitCreateForm);
-  form.querySelector("#create-cancel-btn").addEventListener("click", () => {
-    form.removeEventListener("submit", submitCreateForm);
-    form.parentElement.close();
-    form.reset();
-  });
+    const form = document.querySelector("#create-form");
+    form.parentElement.showModal();
+    form.addEventListener("submit", submitCreateForm);
+    form.querySelector("#create-cancel-btn").addEventListener("click", () => {
+        form.removeEventListener("submit", submitCreateForm);
+        form.parentElement.close();
+        form.reset();
+    });
 }
