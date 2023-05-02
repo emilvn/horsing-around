e719c153851b03e2064cd2e9daf68d4b8cd00826
emilvn <emilvnielsen@hotmail.com>
2023-05-02 15:24:00 +0200

    removed comment from validation file

--- js/modules/validation/validation.js
+++ js/modules/validation/validation.js
@@ -1,6 +1,5 @@
 /* ========== VALIDATE PASSWORD ========== */
 export function validatePassword(pwd) {
-    /*const password = "emptyyourselfinsideme123";*/
     const password = "admin123";
     return pwd === password;
 }
