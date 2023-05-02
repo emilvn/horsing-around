3a94679aa0321742f3e2d92a748375e491bd95dd
emilvn <emilvnielsen@hotmail.com>
2023-05-02 23:38:45 +0200

    added http response codes to error messages

--- js/main.js
+++ js/main.js
@@ -11,118 +11,131 @@ import { sortHorses } from "./modules/searchAndSort/sort.js";
 window.addEventListener("load", main);
 
 /* ===== Global variables ===== */
-export const endpoint =
-  "https://gallopgalore-80085-default-rtdb.europe-west1.firebasedatabase.app/";
+export const endpoint = "https://gallopgalore-80085-default-rtdb.europe-west1.firebasedatabase.app/";
 
 async function main() {
 
-  /* Show horses */
-  await updateGrid();
+    /* Show horses */
+    await updateGrid();
 
-  /* Create dialog */
-  document
-    .querySelector("#add-horse-dialog-button")
-    .addEventListener("click", showCreateDialog);
+    /* Create dialog */
+    document.querySelector("#add-horse-dialog-button")
+        .addEventListener("click", showCreateDialog);
 
-  /* Search event listeners */
-  document
-    .querySelector("#searchBar")
-    .addEventListener("search", inputSearchChanged);
-  document
-    .querySelector("#searchBar")
-    .addEventListener("keyup", inputSearchChanged);
+    /* Search event listeners */
+    const searchBar =  document.querySelector("#searchBar");
+    searchBar.addEventListener("search", inputSearchChanged);
+    searchBar.addEventListener("keyup", inputSearchChanged);
 
     /* close delete dialog */
-    document.querySelector("#delete-cancel-btn").addEventListener("click", closeDeleteDialog);
+    document.querySelector("#delete-cancel-btn")
+        .addEventListener("click", closeDeleteDialog);
 
-     /* sort horses on change in sort menu */
-     
-     document.querySelector("#sortBy").addEventListener("change", sortHorses);
-     
+    /* sort horses on change in sort menu */
+    document.querySelector("#sortBy")
+        .addEventListener("change", sortHorses);
 }
-
 /* ========== CREATE ========== */
 export async function addHorse(horseObj, endpoint) {
-  const json = JSON.stringify(horseObj);
-  const response = await fetch(`${endpoint}/horses.json`, {
-    method: "POST",
-    body: json,
-  });
-  if (response.ok) {
-    console.log("nice");
-    await updateGrid();
-    showToastMessage("Horse added, nice", "success");
-  } else {
-    showToastMessage("not nice", "error");
-  }
+    try{
+        const response = await fetch(`${endpoint}/horses.json`, {
+            method: "POST",
+            body: JSON.stringify(horseObj),
+        });
+        if (response.ok) {
+            await updateGrid();
+            showToastMessage("Horse added, nice", "success");
+        } else {
+            showToastMessage(`Oops something went wrong. ${response.status} ${response.statusText}.`, "error");
+            console.error(`Bad response at addHorse: ${response.status} ${response.statusText}.`);
+        }
+    }
+    catch (err){
+        throw new Error(`Error at addHorse: ${err}`);
+    }
 }
-
 /* ========== READ ALL========== */
 export async function getHorses(endpoint) {
-  const response = await fetch(`${endpoint}/horses.json`); // fetch request, (GET)
-  const data = await response.json(); // parse JSON to JavaScript
-  const horses = prepareData(data); // convert object of object to array of objects
-  console.log("getHorsesKører");
-  return horses;
+    try{
+        const response = await fetch(`${endpoint}/horses.json`);
+        if(response.ok){
+            const data = await response.json();
+            return prepareData(data);
+        }
+        else{
+            showToastMessage(`Oops something went wrong. ${response.status} ${response.statusText}.`, "error");
+            console.error(`Bad response at getHorses: ${response.status} ${response.statusText}.`);
+        }
+    }
+    catch (err){
+        throw new Error(`Error at getHorses: ${err}`);
+    }
 }
-
 /* ========== Data preparation for getHorses ========== */
-// use in getHorses to change the fetched data from object to array.
 function prepareData(obj) {
-  const dataArr = [];
-  for (const key in obj) {
-    const horse = obj[key];
-    horse["id"] = key;
-    dataArr.push(horse);
-  }
-  return dataArr;
+    const dataArr = [];
+    for (const key in obj) {
+        const horse = obj[key];
+        horse["id"] = key;
+        dataArr.push(horse);
+    }
+    return dataArr;
 }
-
 /* ========== READ ONE ========== */
 export async function getOneHorse(horseID, endpoint) {
-  try {
-    const response = await fetch(`${endpoint}horses/${horseID}.json`);
-    if (response.ok) {
-      const horse = await response.json();
-      horse["id"] = horseID;
-      return horse;
+    try {
+        const response = await fetch(`${endpoint}horses/${horseID}.json`);
+        if (response.ok) {
+        const horse = await response.json();
+        horse["id"] = horseID;
+        return horse;
+        }
+        else{
+            showToastMessage(`Oops something went wrong. ${response.status} ${response.statusText}.`, "error");
+            console.error(`Bad response at getOneHorse: ${response.status} ${response.statusText}.`);
+        }
+    } catch (err) {
+        throw new Error(`Error at getOneHorse: ${err}`);
     }
-  } catch (err) {
-    throw new Error(`Error at getOneHorse: ${err}`);
-  }
 }
-
 /* ========== UPDATE ========== */
 // Sends put request to endpoint with horse object
 export async function updateHorse(horse, horseID, endpoint) {
-  try {
-    const response = await fetch(`${endpoint}horses/${horseID}.json`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(horse),
-    });
-    if (response.ok) {
-      console.log("Horse updated successfully!");
-      showToastMessage("Horse updated successfully!", "success");
-    } else {
-      showToastMessage("Failed to update Horse.", "error");
+    try {
+        const response = await fetch(`${endpoint}horses/${horseID}.json`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(horse),
+        });
+        if (response.ok) {
+            console.log("Horse updated successfully!");
+            showToastMessage("Horse updated successfully!", "success");
+        } else {
+            showToastMessage(`Oops something went wrong. ${response.status} ${response.statusText}.`, "error");
+            console.error(`Bad response at updateHorse: ${response.status} ${response.statusText}.`);
+        }
+    } catch (err) {
+        throw new Error(`Error at updateHorse: ${err}`);
     }
-  } catch (err) {
-    throw new Error(`Error at updateHorse: ${err}`);
-  }
 }
 
 /* ========== DELETE ========== */
 export async function deleteHorse(horseID, endpoint) {
-  const response = await fetch(`${endpoint}/horses/${horseID}.json`, {
-    method: "DELETE",
-  });
-  if (response.ok) {
-    console.log("horse deleted");
-    showToastMessage("Horse deleted successfully!", "success");
-    await updateGrid();
-  } else {
-    console.log("Bad response at deleteHorse");
-    showToastMessage("Couldn't delete horse.", "error");
-  }
+    try{
+
+        const response = await fetch(`${endpoint}/horses/${horseID}.json`, {
+            method: "DELETE",
+        });
+        if (response.ok) {
+            console.log("horse deleted");
+            showToastMessage("Horse deleted successfully!", "success");
+        await updateGrid();
+        } else {
+            showToastMessage(`Oops something went wrong. ${response.status} ${response.statusText}.`, "error");
+            console.error(`Bad response at deleteHorse: ${response.status} ${response.statusText}.`);
+        }
+    }
+    catch (err){
+        throw new Error(`Error at deleteHorse: ${err}`);
+    }
 }
